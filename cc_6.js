40f20b4efc5dc730f2fe026f471b0e8a5c6f924d
henriquesenaribeiro <henriqueribeiro@usf.edu>
2025-02-09 12:34:53 -0500

    Task 2: Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -9,3 +9,16 @@ calculateProfit(20, 30, 100);
 calculateProfit(50, 70, 200);
 
 // Created a formula to calculate profit based on three given parameters.
+
+// Task 2: Sales Tax Computation
+
+function calculateSalesTax(amount, taxRate) {
+    let tax = amount * taxRate;
+    console.log(`Sales Tax: $${tax}`);
+
+};
+
+calculateSalesTax(100, 0.07); // Expected output: "Sales Tax: $7"
+calculateSalesTax(500, 0.1);  // Expected output: "Sales Tax: $50"
+
+// Developed a function with two inputs to compute sales tax.
