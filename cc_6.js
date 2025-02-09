0d7240e40afa03df1eda363fa2ef3aa75a054895
henriquesenaribeiro <henriqueribeiro@usf.edu>
2025-02-09 12:33:36 -0500

    Task 1: Business Profit Calculation

--- cc_6.js
+++ cc_6.js
@@ -1,1 +1,11 @@
+// Task 1: Business Profit Calculation
 
+function calculateProfit(costPrice, sellingPrice, unitsSold) {
+    let profit = (sellingPrice - costPrice) * unitsSold;
+    console.log(`Total Profit: $${profit}`);
+}
+
+calculateProfit(20, 30, 100);
+calculateProfit(50, 70, 200);
+
+// Created a formula to calculate profit based on three given parameters.
