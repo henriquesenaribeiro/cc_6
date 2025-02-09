eb52e9ebb6d7c6cb9d8fcb9e3f949ac93fc61ded
henriquesenaribeiro <henriqueribeiro@usf.edu>
2025-02-09 12:36:17 -0500

    Task 3: Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -22,3 +22,19 @@ calculateSalesTax(100, 0.07); // Expected output: "Sales Tax: $7"
 calculateSalesTax(500, 0.1);  // Expected output: "Sales Tax: $50"
 
 // Developed a function with two inputs to compute sales tax.
+
+// Task 3: Employee Bonus Calculation
+
+const calculateBonus = (salary, performanceRating) => 
+    performanceRating === "Excellent" ? salary * 0.2 :
+    performanceRating === "Good" ? salary * 0.1 :
+    performanceRating === "Average" ? salary * 0.05 :
+    0;
+
+console.log(`Bonus: $${calculateBonus(5000, "Excellent")}`); // "Bonus: $1000"
+console.log(`Bonus: $${calculateBonus(7000, "Good")}`); // "Bonus: $700"
+
+calculateBonus(5000, "Excellent"); // Expected output: "Bonus: $1000"
+calculateBonus(7000, "Good");      // Expected output: "Bonus: $700"
+
+// Based on the perfomance, bonus could be calculated
